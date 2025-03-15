df6728607c9dc66bc8434f7fe7f01b672d50b8ff
Sebastian Pytka <76405637+Fablek@users.noreply.github.com>
2025-03-15 22:40:28 +0100

    :wrench: Add ProductAddons table

---
+++ FoodDeliveryWebsite/Models/ProductAddon.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoodDeliveryWebsite.Models
+{
+    public class ProductAddon
+    {
+        public int ProductAddonId { get; set; }
+
+        [ForeignKey("ProductId")]
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        [ForeignKey("AddonId")]
+        public int AddonId{ get; set; }
+        public Addon Addon { get; set; }
+    }
+}
